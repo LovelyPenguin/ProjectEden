5710718463ff86351a2fed3dcccc834fb6d9b69a
lyp3238 <lyp3238@gmail.com>
2020-03-11 21:43:50 +0900

    버그 수정

--- Assets/Scripts/Monster/Boss/StateMachineBehaviour/BossRageKick.cs
+++ Assets/Scripts/Monster/Boss/StateMachineBehaviour/BossRageKick.cs
@@ -23,6 +23,7 @@ public class BossRageKick : StateMachineBehaviour
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        animator.SetBool("RageOut", false);
         animator.GetComponent<VL_P1_Chopping>().InitializeState();
     }
 
